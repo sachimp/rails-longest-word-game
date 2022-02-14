0e6329569c4e0ef9f883c0d38c955dd822a24e5c
Sachin Gandhi <sachingandhi@live.com>
2022-02-14 17:54:05 +0000

    implemented the valid/invalid checker on the word that gets submitted

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
+    @letters = ("a".."z").to_a.sample(10)
   end
 
   def score
+  @letters = params[:letters]
+  @word = params[:word]
+  response = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
+  json = JSON.parse(response.read)
+
+
+  if @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) } && json["found"] == true
+    @result = "valid"
+  elsif @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) } && json["found"] == false
+    @result = "in grid but not a word"
+  else
+    @result = "invalid"
+  end
   end
 end
